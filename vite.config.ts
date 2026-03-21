3482bd6c4c0e037aa6e4076f636df133e03bacc5
kelpy <itskelpy@proton.me>
2026-03-21 22:25:30 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,85 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'node:path'
+import { readdirSync } from 'node:fs'
+import { VuetalePlugin } from './.vuetale/VuetalePlugin'
+import vuetaleConfig from './lib/vuetale-plugin.json'
+
+
+function getComponentEntries() {
+  const componentsDir = resolve(__dirname, 'lib/components')
+  return readdirSync(componentsDir)
+    .filter(file => file.endsWith('.vue'))
+    .reduce((entries, file) => {
+      const name = file.replace('.vue', '')
+      entries[name] = resolve(componentsDir, file)
+      return entries
+    }, {} as Record<string, string>)
+}
+
+function getScriptEntries() {
+  const scriptsDir = resolve(__dirname, 'lib')
+  return readdirSync(scriptsDir)
+    .filter(file => file.endsWith('.ts'))
+    .reduce((entries, file) => {
+      const name = file.replace('.ts', '')
+      entries[name] = resolve(scriptsDir, file)
+      return entries
+    }, {} as Record<string, string>)
+}
+
+
+function getPageEntries() {
+  const pagesDir = resolve(__dirname, 'lib/pages')
+  return readdirSync(pagesDir)
+    .filter(file => file.endsWith('.vue'))
+    .reduce((entries, file) => {
+      const name = file.replace('.vue', '')
+      entries[name] = resolve(pagesDir, file)
+      return entries
+    }, {} as Record<string, string>)
+}
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    VuetalePlugin()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./lib', import.meta.url))
+    },
+  },
+  build: {
+
+    lib: {
+      entry: {
+        // you can still have a main barrel if you want
+        // index: resolve(__dirname, 'src/index.ts'),
+        ...getPageEntries(),   // Button, Card, Modal, ...
+        ...getComponentEntries(),
+        ...getScriptEntries()
+      },
+      formats: ['es'],
+      fileName: (_, entryName) => `${entryName}.js`
+    },
+
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        preserveModules: false,   // usually false here — each entry becomes one file
+        entryFileNames: '[name].js',
+      }
+    },
+
+    outDir: '../main/resources/vuetale/' + vuetaleConfig.name,
+    emptyOutDir: true
+  }
+})
